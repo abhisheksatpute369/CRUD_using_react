9193222165b00eb37ae996221df20e7a9be9fc7f
abhisheksatpute369 <abhisheksatpute99@gmail.com>
2022-05-29 12:02:38 +0530

    create basic structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import Adduser from './components/adduser';
+import Edituser from './components/edituser';
+import Navbar from './components/navbar';
+import User from './components/user';
+import Users from './components/users';
 
 function App() {
   return (
     <div>
-      
+        <Navbar />
+        <Users />
+        <User />
+        <Edituser />
+        <Adduser />
     </div>
   );
 }
---
+++ src/components/adduser.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Adduser = () => {
+    return (
+        <div>
+            Add
+        </div>
+    );
+};
+
+export default Adduser;
